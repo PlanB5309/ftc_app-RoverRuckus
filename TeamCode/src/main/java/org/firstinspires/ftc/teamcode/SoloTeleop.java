7be67d8a735f02a3b4717c70ade4d33e07591bb1
Tony Congdon <congdontony@yahoo.com>
2018-11-04 15:26:01 -0500

    Updating SoloTeleop with magic.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoloTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoloTeleop.java
@@ -7,30 +7,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @TeleOp(name="Solo Teleop", group="Robot")
-public class SoloTeleop extends OpMode {
+public class SoloTeleop extends OpMode{
 
     RobotHardware robot = new RobotHardware(telemetry);
+    boolean sweeperStatus = false;
+    boolean direction = true;
 
 
     public void init () {
         robot.init(hardwareMap);
     }
-    public void loop () {
+    public void loop() {
 
         //Set driving controls on gamepad 1
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-        robot.leftDrive.setPower(leftPower);
-        robot.rightDrive.setPower(rightPower);
+
+        robot.leftDrive.setPower(gamepad1.left_stick_y);
+        robot.rightDrive.setPower(gamepad1.right_stick_y);
 
         //Opening and Closing lifter claws with left bumper+trigger
-        if (gamepad1.left_bumper) {
+        if (gamepad1.b) {
             robot.leftClaw.setPosition(robot.LEFT_CLAW_OPEN);
             robot.rightClaw.setPosition(robot.RIGHT_CLAW_OPEN);
         }
-        if (gamepad1.left_trigger > 0.5) {
+        if (gamepad1.y) {
             robot.leftClaw.setPosition(robot.LEFT_CLAW_CLOSED);
             robot.rightClaw.setPosition(robot.RIGHT_CLAW_CLOSED);
         }
@@ -47,15 +46,24 @@ public class SoloTeleop extends OpMode {
         }
 
         //Setting the mineral sweeper motor settings
-        if (gamepad1.a) {
-            robot.sweeperMotor.setPower(1);
+
+        if(gamepad1.start){
+            sweeperStatus = !sweeperStatus;
         }
-        else if (gamepad1.y) {
-            robot.sweeperMotor.setPower(-1);
+        if(gamepad1.a)
+            direction = false;
+        if(gamepad1.x)
+            direction = true;
+        if(sweeperStatus){
+            if(direction){
+                robot.sweeperMotor.setPower(1);
+            }
+            else{
+                robot.sweeperMotor.setPower(-1);
+            }
         }
-        else {
+        else
             robot.sweeperMotor.setPower(0);
-        }
 
     }
 }
