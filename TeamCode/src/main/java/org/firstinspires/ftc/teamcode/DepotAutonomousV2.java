5f35d192ab099343f906f2c52affb8d1a70611e4
Tony Congdon <congdontony@yahoo.com>
2019-01-20 14:29:11 -0500

    Creation of DepotAutonomousV2. Minor chanes in original program to back off the team marker.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotAutonomousV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotAutonomousV2.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-@Autonomous(name = "Depot Autonomous", group = "Auto")
+@Autonomous(name = "Depot Autonomous V2", group = "Auto")
 
 
-public class DepotAutonomous extends LinearOpMode {
+public class DepotAutonomousV2 extends LinearOpMode {
     RobotHardware robot = new RobotHardware(telemetry);
     LowerRobot lowerRobot = new LowerRobot(robot, telemetry);
     OpenHooks openHooks = new OpenHooks(robot, telemetry);
@@ -37,15 +36,20 @@ public class DepotAutonomous extends LinearOpMode {
         if (goldPosition == robot.LEFT) {
             gyroTurn.left(20);
             robot.sweeperMotor.setPower(-0.5);
-            drive.forward(0.5, 33);
-            gyroTurn.right(55);
-            drive.forward(0.5, 12);
+            drive.forward(0.5, 24);
             robot.sweeperMotor.setPower(0);
+            gyroTurn.right(15);
+            drive.forward(0.5, 20);
             robot.bucketServo.setPosition(0.58);
             Thread.sleep(1000);
             robot.sweeperMotor.setPower(-0.5);
-            drive.backward(0.5, 12);
+            Thread.sleep(2000);
             robot.sweeperMotor.setPower(0);
+            drive.backward(0.5, 36);
+            gyroTurn.left(90);
+            drive.backward(0.5, 60);
+            gyroTurn.right(45);
+            drive.backward(0.5, 28);
 //            gyroTurn.right(18);
 //            drive.backward(0.5, 56);
         } else if (goldPosition == robot.RIGHT) {
@@ -53,24 +57,25 @@ public class DepotAutonomous extends LinearOpMode {
             robot.sweeperMotor.setPower(-0.5);
             drive.forward(0.5, 30);
             robot.sweeperMotor.setPower(0);
-            gyroTurn.left(65);
-            drive.forward(0.5, 18);
-            robot.bucketServo.setPosition(0.58);
-            Thread.sleep(1000);
-            robot.sweeperMotor.setPower(-0.5);
-            drive.backward(0.5, 12);
-            robot.sweeperMotor.setPower(0);
+            drive.backward(0.5, 20);
+            gyroTurn.left(155);
+            drive.backward(0.5, 24);
         } else {
             robot.sweeperMotor.setPower(-0.5);
-            drive.forward(0.5, 30);
+            drive.forward(0.5, 40);
             robot.sweeperMotor.setPower(0);
-            drive.forward(0.5, 22);
             robot.bucketServo.setPosition(0.58);
             Thread.sleep(1000);
             robot.sweeperMotor.setPower(-0.5);
-            drive.backward(0.5, 12);
+            Thread.sleep(2000);
             robot.sweeperMotor.setPower(0);
+            drive.backward(0.5, 22);
+            gyroTurn.right(90);
+            drive.forward(0.5, 42);
+            gyroTurn.right(45);
+            drive.forward(0.5, 28);
         }
+        mineralLift.setDown();
         while(isStopRequested() == false){
 
         }
